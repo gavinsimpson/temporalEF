ce2a8745e10f5a2b9dd20e433130d74848cf8d72
Gavin Simpson <ucfagls@gmail.com>
2015-05-05 21:43:04 -0600

    * fix calculation of p value;always return a parametric p value; arguments changed and moved around; adds a print method  * update plot method to show significance of I

--- R/moranI.R
+++ R/moranI.R
@@ -19,15 +19,22 @@
 
 ##' @rdname moranI
 ##'
+##' @param permute logical; test \eqn{I} via a permutation test
+##' @param alternative character; the type of test to perform. The default is \code{"two.sided"}, which allows for both positive and negative spatial correlation (values of \eqn{I}). If you anticipate only one of positive or negative spatial association then you can specify \code{"greater"} or \code{"less"}, respectively, as the alternative hypothesis.
+##' @param nperm numeric; number of permutations to perform in permutation test if requested.
+##'
 ##' @export
-`moranI.ate` <- function(x, ...) {
+`moranI.ate` <- function(x, permute = FALSE,
+                         alternative = c("two.sided", "greater", "less"),
+                         nperm = 999, ...) {
     v <- x$vectors
     n <- NROW(v)
     w <- x$weights
     if (attr(w, "link0")) {
         w <- w[-1]
     }
-    stat <- apply(v, 2, calcMoranI, w = w, ...)
+    alternative <- match.arg(alternative)
+    stat <- apply(v, 2, calcMoranI, w = w, permute = permute, ...)
     stat <- do.call(rbind, stat)
 
     ## expected value of Moran's I
@@ -38,9 +45,40 @@
     out
 }
 
+##' @export
+`print.moranI` <- function(x,
+                           digits = max(3, getOption("digits") - 2),
+                           eps.Pvalue = .Machine$double.eps,
+                           na.print = "NA",
+                           signif.stars = getOption("show.signif.stars"),
+                           signif.legend = signif.stars, ...) {
+    cat("\n")
+    writeLines(strwrap("Moran's I of Temporal Eigenfunctions"))
+    cat("\n")
+    stat <- unlist(x$statistic[,1])
+    pval <- unlist(x$statistic[,2])
+    statistic <- cbind("Moran's I" = stat, "p value" = pval)
+    statistic[,1] <- format(round(unlist(statistic[,1]), digits = digits),
+                            digits = digits)
+    statistic[,2] <- format.pval(pval, digits = digits, eps = eps.Pvalue)
+    Signif <- symnum(pval, corr = FALSE, na = FALSE,
+                     cutpoints = c(0, 0.001, 0.01, 0.05, 0.1, 1),
+                     symbols = c("***", "**", "*", ".", " "))
+    statistic <- cbind(statistic, format(Signif))
+    print.default(statistic, quote = FALSE, right = TRUE, na.print = na.print,
+                  ...)
+    if (signif.stars && signif.legend) {
+        if ((w <- getOption("width")) < nchar(sleg <- attr(Signif, "legend")))
+            sleg <- strwrap(sleg, width = w - 2, prefix = "  ")
+        cat("---\nSignif. codes:  ", sleg, sep = "", fill = w +
+            4 + max(nchar(sleg, "bytes") - nchar(sleg)))
+    }
+    invisible(x)
+}
+
 `calcMoranI` <- function(y, w, scale = FALSE, norm = FALSE,
-                         test = c("none", "parametric", "permutation"),
-                         alternative = c("two.sided", "greater", "less"),
+                         permute = FALSE,
+                         alternative = "two.sided",
                          nperm = 999) {
     ## Implementation based on equations in Gittleman & Kot (1990) Systematic
     ## Zoology 39(3):227--241
@@ -78,14 +116,11 @@
     expI <- -1 / (n - 1)
 
     ## test Moran's I
-    test <- match.arg(test)
-    alternative <- match.arg(alternative)
-
     pval <- NULL
     ## parametric, assuming I is distributed Gaussian
-    if (isTRUE(all.equal(test, "parametric"))) {
-        alternative <- if (alternative %in% c("less","greater")) {
-            c("less","greater")[(obsI > expI) + 1]
+    if (!isTRUE(permute)) {
+        if (alternative %in% c("less","greater")) {
+            alternative <- c("less","greater")[(obsI > expI) + 1]
         }
         s1 <- 0.5 * sum((W + t(W))^2)                ## G & K Eqn (7)
         s2 <- sum((rowSums(W) + colSums(W))^2)       ## G & K Eqn (8)
@@ -96,20 +131,13 @@
         sdev <- sqrt(sdev - (1 / ((n - 1)^2)))       ## G & K Eqn (6)
         ## pval
         if (isTRUE(all.equal(alternative, "two.sided"))) {
-            pval <- if (obsI <= expI) {
-                2 * pnorm(obsI, mean = expI, sd = sdev, lower.tail = FALSE)
-            } else {
-                2 * pnorm(obsI, mean = expI, sd = sdev)
-            }
+            pval <- 2 * pnorm(-abs(obsI), mean = expI, sd = sdev)
         } else if (isTRUE(all.equal(alternative, "greater"))) {
             pval <- pnorm(obsI, mean = expI, sd = sdev, lower.tail = FALSE)
         } else {
             pval <- pnorm(obsI, mean = expI, sd = sdev)
         }
-    }
-
-    ## permutation-based
-    if (isTRUE(all.equal(test, "permutation"))) {
+    } else if (isTRUE(permute)) {
         alternative <- if (isTRUE(all.equal(alternative, "two.sided"))) {
             c("less","greater")[(obsI > expI) + 1]
         }
@@ -125,6 +153,8 @@
             sum(permI <= obsI) / (nperm + 1)
         } else if (isTRUE(all.equal(alternative, "greater"))) {
             sum(permI >= obsI) / (nperm + 1)
+        } else {
+            sum(abs(perm) >= abs(obsI))
         }
     }
 
@@ -134,10 +164,14 @@
 ##'
 ##' @description A plot of Moran's I versus time.
 ##' @param x an object of class \code{\link{moranI}}.
+##' @param alpha numeric; level of significance
 ##' @param type character; the type of plotting to use. See
 ##' \code{\link{plot.default}}.
 ##' @param xlab the label for the x-axis of the plot.
 ##' @param ylab the label for the y-axis of the plot.
+##' @param pch the plotting character to use
+##' @param bg fill colour of points
+##' @param col border colour of points
 ##' @param ... additional arguments passed to \code{\link{plot}}.
 ##'
 ##' @return A plot on the current device
@@ -145,14 +179,21 @@
 ##' @author Gavin L. Simpson
 ##'
 ##' @export
-`plot.moranI` <- function(x, type = "b", xlab = "Eigenfunctions",
-                          ylab = "Moran's I", ...) {
+`plot.moranI` <- function(x, alpha = 0.05,
+                          type = "b",
+                          xlab = "Eigenfunctions", ylab = "Moran's I",
+                          pch = 21, bg = "red", col = "black", ...) {
     statistic <- x$statistic[,1]
+    signif <- x$statistic[,2] > alpha
+    n <- NROW(statistic)
+    col <- rep(col, length.out = n)
+    bg <- rep(bg, length.out = n)
+    bg[signif] <- "transparent"
     xvals <- seq_along(statistic)
     plot(xvals, statistic, type = "n", ..., axes = FALSE,
-         ylab = ylab, xlab = xlab)
-    abline(h = x$expected, col = "red")
-    points(xvals, statistic, type = type)
+         ylab = ylab, xlab = xlab,
+         panel.first = abline(h = x$expected, col = "red"))
+    points(xvals, statistic, type = type, pch = pch, col = col, bg = bg, ...)
     axis(side = 2)
     axis(side = 1, labels = paste0("EF", xvals), at = xvals)
     box()
