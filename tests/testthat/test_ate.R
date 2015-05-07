13c2f44d3a0785708e68891a286abd4b5c66b430
Gavin Simpson <ucfagls@gmail.com>
2015-05-06 22:39:07 -0600

    more test; specifically for error message when too short a time series added

--- tests/testthat/test_ate.R
+++ tests/testthat/test_ate.R
@@ -8,3 +8,8 @@ test_that("ate() returns correct object", {
     tefs <- ate(tp)
     expect_is(tefs, "ate")
 })
+
+test_that("ate handles inputs correctly; length", {
+    expect_is(ate(1:2), "ate")
+    expect_error(ate(1), "At least two time points are required for ATE.")
+})
